9b0390156bdcdc1cde7b8f5366753da2580092f8
Ivan Karvyalis <ivankvl7@gmail.com>
2024-07-17 11:35:25 +0600

    v2 accounts, blog apps and bootstrap

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+from django.conf import settings
+from django.conf.urls.static import static
+
+sitemaps = {
+    'posts': PostSitemap,
+}
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog'))
-]
+    path('', include('blog.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
+    path("accounts/", include('accounts.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
